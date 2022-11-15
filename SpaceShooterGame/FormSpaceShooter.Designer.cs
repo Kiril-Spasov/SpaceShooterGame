14fd1ee98560d59748372c1dafe87a6536aac7b4
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-15 11:37:31 +0200

    Add project files.

---
+++ SpaceShooterGame/FormSpaceShooter.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SpaceShooterGame
+{
+    partial class FormSpaceShooter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSpaceShooter));
+            this.TimerSpaceShip = new System.Windows.Forms.Timer(this.components);
+            this.TimerRocket = new System.Windows.Forms.Timer(this.components);
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.TimerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.BtnPause = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimerSpaceShip
+            // 
+            this.TimerSpaceShip.Enabled = true;
+            this.TimerSpaceShip.Tick += new System.EventHandler(this.TimerSpaceShip_Tick);
+            // 
+            // TimerRocket
+            // 
+            this.TimerRocket.Tick += new System.EventHandler(this.TimerRocket_Tick);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(18, 27);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(102, 40);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(126, 27);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(100, 39);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // TimerCountdown
+            // 
+            this.TimerCountdown.Interval = 1000;
+            this.TimerCountdown.Tick += new System.EventHandler(this.TimerCountdown_Tick);
+            // 
+            // BtnPause
+            // 
+            this.BtnPause.Location = new System.Drawing.Point(232, 27);
+            this.BtnPause.Name = "BtnPause";
+            this.BtnPause.Size = new System.Drawing.Size(88, 39);
+            this.BtnPause.TabIndex = 2;
+            this.BtnPause.Text = "Pause";
+            this.BtnPause.UseVisualStyleBackColor = true;
+            this.BtnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(340, 29);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(90, 37);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // FormSpaceShooter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 669);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.BtnPause);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.KeyPreview = true;
+            this.Name = "FormSpaceShooter";
+            this.Text = "Space Shooter";
+            this.Load += new System.EventHandler(this.FormSpaceShooter_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormSpaceShooter_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormSpaceShooter_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerSpaceShip;
+        private System.Windows.Forms.Timer TimerRocket;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Timer TimerCountdown;
+        private System.Windows.Forms.Button BtnPause;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ SpaceShooterGame/FormSpaceShooter.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Media;
+
+
+namespace SpaceShooterGame
+{
+    public partial class FormSpaceShooter : Form
+    {
+        public FormSpaceShooter()
+        {
+            InitializeComponent();
+            //Use double buffering to reduce the flicker.
+            this.DoubleBuffered = true;
+        }
+
+        //Store images
+        Image[] spaceShipImages = new Image[12];
+        Image stones;
+        Image rocket;
+        Image background;
+        Image spaceShip;
+        Image explosion;
+
+        //Used for random positions.
+        Random r = new Random();
+
+        SoundPlayer explosionSound = new SoundPlayer();
+
+        //Used for spaceship animation counter.
+        int count = 0;
+
+        bool hit;
+
+        //Store position and directions.
+        int spaceShipX, spaceShipY;
+        int rocketX, rocketY, rocketDirY;
+        int stonesX, stonesY, stonesDirX, stonesDirY;
+        int explosionX, explosionY;
+
+        int score;
+        int timeLeft;
+        bool startFlag;
+        bool gameOver;
+
+        Font font = new Font("Verdana", 12, FontStyle.Bold);
+        Font endGame = new Font("Verdana", 24, FontStyle.Bold);
+        SolidBrush brush = new SolidBrush(Color.Yellow);
+
+        private void FormSpaceShooter_Load(object sender, EventArgs e)
+        {
+            //Load images.
+            for (int i = 0; i <= 11; i++)
+            {
+                spaceShipImages[i] = Image.FromFile(Application.StartupPath + @"\space-ship-images\spaceship" + i.ToString() + "-removebg-preview.png");
+            }
+            spaceShip = spaceShipImages[0];
+
+            background = Image.FromFile(Application.StartupPath + @"\space-background.png");
+            stones = Image.FromFile(Application.StartupPath + @"\stones.png");
+            rocket = Image.FromFile(Application.StartupPath + @"\rocket.png");
+            explosion = Image.FromFile(Application.StartupPath + @"\explosion.png");
+
+            //Load sound.
+            explosionSound.SoundLocation = Application.StartupPath + @"\explosion.wav";
+            explosionSound.LoadAsync();
+
+            //Set start pos for the spaceship.
+            spaceShipX = this.Width / 3;
+            spaceShipY = this.Height - spaceShip.Height - 75;
+
+            //Set pos of the screen.
+            rocketX = -100;
+
+            //Direction is set to 0 until we pres the button to shoot.
+            rocketDirY = 0;
+
+            //Set pos off the screen.
+            explosionX = -100;
+
+            //Set random initial X pos.
+            stonesX = r.Next(0, this.Width);
+            stonesY = 0;
+
+            //Set random initial X direction.
+            stonesDirX = r.Next(-15, 15);
+            stonesDirY = 15;
+
+            hit = false;
+            startFlag = false;
+        }
+
+        private void FormSpaceShooter_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            DrawGame(g);
+        }
+
+        private void DrawGame(Graphics g)
+        {
+            g.DrawImage(background, 0, 0);
+            g.DrawString($"Score: " + score.ToString(), font, brush, 400, 10);
+            g.DrawString($"Time left: " + timeLeft.ToString(), font, brush, 500, 10);
+
+            g.DrawImage(spaceShip, spaceShipX, spaceShipY, 200, 150);
+            g.DrawImage(stones, stonesX, stonesY);
+            g.DrawImage(rocket, rocketX, rocketY, 45, 45);
+            
+            if (hit)
+            {
+                g.DrawImage(explosion, explosionX, explosionY, 45, 45);
+                explosionSound.Play();
+                //Set the initial values.
+                Initialize();
+            }
+
+            if (gameOver)
+            {
+                g.DrawString($"GAME OVER", endGame, brush, 240, 180);
+                g.DrawString($"(Press Start button to play again.)", font, brush, 200, 230);
+            }
+        }
+
+        private void Initialize()
+        {
+            rocketX = -100;
+            rocketDirY = 0;
+
+            explosionX = -100;
+
+            stonesX = r.Next(0, 500);
+            stonesY = 0;
+
+            stonesDirX = r.Next(-15, 15);
+            stonesDirY = 15;
+
+            hit = false;
+        }
+
+        private void FormSpaceShooter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (startFlag)
+            {
+                if (e.KeyCode == Keys.A)
+                {
+                    spaceShipX -= 5;
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    spaceShipX += 5;
+                }
+
+                if (e.KeyCode == Keys.W)
+                {
+                    rocketX = spaceShipX + 77;
+                    rocketY = spaceShipY - 15;
+                    rocketDirY = 12;
+                }
+            }
+            Invalidate();
+        }
+
+        //Timer for rocket and stones movement.
+        private void TimerRocket_Tick(object sender, EventArgs e)
+        {  
+            //Pos Y for the rocket is 0 until we press the button.
+            rocketY += -rocketDirY;
+
+            stonesX += stonesDirX;
+            stonesY += stonesDirY;
+
+            //Check if stones hit the wall, then bounce back.
+            if (stonesX < 0)
+            {
+                stonesDirX = -stonesDirX;
+            }
+            else if (stonesX > ClientRectangle.Width - stones.Width)
+            {
+                stonesDirX = -stonesDirX;
+            }
+            else if (stonesY < 0)
+            {
+                stonesDirY = -stonesDirY;
+            }
+            else if (stonesY > ClientRectangle.Height - stones.Height)
+            {
+                stonesDirY = -stonesDirY;
+            }
+
+            //Check if rocket reached the top without collision.
+            if (rocketY < 0)
+            {
+                rocketX = -100;
+                rocketDirY = 0;
+            }
+            //Check for collision.
+            else if ((rocketX < stonesX + stones.Width) && (rocketX + rocket.Width > stonesX) && (rocketY + rocket.Height > stonesY) && (stonesY + stones.Height > rocketY) && hit == false)
+            {
+                hit = true;
+                score++;
+                explosionX = stonesX;
+                explosionY = stonesY;
+            }
+            Invalidate();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            timeLeft = 60;
+            TimerRocket.Enabled = true;
+            TimerCountdown.Enabled = true;
+            startFlag = true;
+            gameOver = false;
+            axWindowsMediaPlayer1.URL = Application.StartupPath + @"\spacesound.mp3";
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            GameStop();
+            axWindowsMediaPlayer1.close();
+            Initialize();
+        }
+
+        private void TimerCountdown_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            if (timeLeft == 0)
+            {
+                GameStop();
+                Initialize();
+            }
+        }
+
+        private void GameStop()
+        {
+            score = 0;
+            timeLeft = 60;
+            gameOver = true;
+            TimerRocket.Enabled = false;
+            TimerCountdown.Enabled = false;
+            startFlag = false;
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            TimerCountdown.Enabled = !TimerCountdown.Enabled;
+            TimerRocket.Enabled = !TimerRocket.Enabled;
+        }
+
+        private void TimerSpaceShip_Tick(object sender, EventArgs e)
+        {
+            spaceShip = spaceShipImages[count];
+            count++;
+            if (count > 11)
+                count = 0;
+
+            Invalidate();
+        }
+    }
+}
